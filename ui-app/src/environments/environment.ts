f249867728728dbf5aebc9f0feee0a0781d57b1f
SOUMYA <soumyaghosh.ghosh@gmail.com>
2026-08-05 22:41:25 +0530

    Integrated health check every 15 seconds.

--- ui-app/src/environments/environment.ts
+++ ui-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiBaseUrl: 'https://bigyanmanchabackend.onrender.com'
-  apiBaseUrl: 'http://localhost:8080'
+  apiBaseUrl: 'https://bigyanmanchabackend.onrender.com'
+  //apiBaseUrl: 'http://localhost:8080'
 };
